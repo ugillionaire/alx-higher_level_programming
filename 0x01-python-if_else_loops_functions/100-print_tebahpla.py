669a6b09fb941361157fc061d5103e9f1ef956ca
Chinemerem Afam-echi <ingeniouschinemerem@gmail.com>
2021-11-24 06:16:50 -0800

    if_else_loops_functions

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for i in range(ord('z'), ord('a'), -2):
+    print("{:c}{:s}".format(i, chr(i - 33)), end="")
+    
---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for x in range(0, 100):
+    if x == 99:
+        print(x)
+    else:
+        print("{:0>2}".format(x), end=", ")
+        
